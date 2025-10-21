4c33d500057b789df6fff1bdfd03bfbbc47afcaf
Nehemia <nehemia.obati@kingsway.co.ke>
2025-10-21 17:39:48 +0300

    hello world

--- codeigniter/app/Config/App.php
+++ codeigniter/app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://afrikenkid.com';
+    public string $baseURL = 'https://www.afrikenkid.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
