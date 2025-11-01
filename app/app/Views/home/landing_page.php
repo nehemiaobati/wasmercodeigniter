3b8ad580146bd19ed08e98dbe93c46498a251700
Nehemia <nehemia.obati@kingsway.co.ke>
2025-11-02 01:40:25 +0300

    hello

--- app/app/Views/home/landing_page.php
+++ app/app/Views/home/landing_page.php
@@ -175,8 +175,8 @@
 <!-- Hero Section -->
 <section class="hero-section">
     <div class="container hero-content">
-        <h1 class="display-3 mb-3">Harness AI & Crypto Data. Unleash Your Ideas.</h1>
-        <p class="lead">Chat with a powerful AI assistant and Query crypto wallets. Top up your account easily with <span style="color: green;">M-Pesa</span>, <span style="color: red;">Airtel Money</span>, or Card. Simple, pay-as-you-go pricing.</p>
+        <h1 class="display-3 mb-3">Generate with Gemini AI & Query Live On-Chain Data. Your Toolkit Awaits.</h1>
+        <p class="lead">Chat with a powerful AI assistant and Unlock on-chain insights for any BTC or LTC address. Top up your account easily with <span style="color: green;">M-Pesa</span>, <span style="color: red;"> Airtel Money</span>, and or Card. Simple, pay-as-you-go pricing.</p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center hero-buttons">
             <a href="<?= url_to('register') ?>" class="btn btn-primary btn-lg px-4 gap-3 fw-bold">Create Your Free Account</a>
             <a href="#features" class="btn btn-outline-light btn-lg px-4">Explore Features</a>
