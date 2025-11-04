06c7c2d18dd1d0b63a9d2cdf386336f4c88b17a0
Nehemia <nehemia.obati@kingsway.co.ke>
2025-11-04 18:02:02 +0300

    hello world

--- app/app/Views/layouts/default.php
+++ app/app/Views/layouts/default.php
@@ -60,7 +60,7 @@
     </script>
 
     <!-- Stylesheets -->
-    <link href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('public/assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
